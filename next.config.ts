27ee32df895e439af3b0cc7207f22fa40438af0e
ck-euan <euan.morgan@countingking.co.uk>
2025-01-03 18:37:59 +0000

    Update Next.js configuration to ignore TypeScript and ESLint errors during builds; refactor imports in page component.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* We lint / typecheck in CI */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
