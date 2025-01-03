eda6a7238e420a4c6820ae89bba535aa5dc45a41
ck-euan <euan.morgan@countingking.co.uk>
2025-01-03 18:36:13 +0000

    init & spaceland helper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,119 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Separator } from "~/components/ui/separator";
+import { cn } from "~/lib/utils";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [initialOrder, setInitialOrder] = useState<Color[]>([]);
+  const [currentOrder, setCurrentOrder] = useState<Color[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold mb-8">
+        Spaceland Boss Fight Simon Says
+      </h1>
+
+      <ColorOrder
+        order={initialOrder}
+        setOrder={setInitialOrder}
+        title="Initial Order"
+      />
+
+      <Separator className="w-full my-8" />
+
+      <ColorOrder
+        order={currentOrder}
+        setOrder={setCurrentOrder}
+        title="Current Order"
+      />
+    </main>
   );
 }
+
+type Color = "red" | "green" | "blue" | "yellow";
+
+const ColorOrder = ({
+  order,
+  setOrder,
+  title,
+}: {
+  order: Color[];
+  setOrder: (order: Color[]) => void;
+  title: string;
+}) => {
+  const listFull = order.length === 4;
+
+  // Create an array of color configs to make the code more DRY
+  const colorConfigs = [
+    {
+      color: "red",
+      bgClass: "bg-rose-500",
+      hoverClass: "hover:bg-rose-600",
+      activeClass: "bg-rose-600",
+    },
+    {
+      color: "green",
+      bgClass: "bg-emerald-500",
+      hoverClass: "hover:bg-emerald-600",
+      activeClass: "bg-emerald-600",
+    },
+    {
+      color: "blue",
+      bgClass: "bg-blue-500",
+      hoverClass: "hover:bg-blue-600",
+      activeClass: "bg-blue-600",
+    },
+    {
+      color: "yellow",
+      bgClass: "bg-yellow-500",
+      hoverClass: "hover:bg-yellow-600",
+      activeClass: "bg-yellow-600",
+    },
+  ] as const;
+
+  // Sort the buttons based on their order when list is full
+  const sortedConfigs = listFull
+    ? [...colorConfigs].sort(
+        (a, b) =>
+          order.indexOf(a.color as Color) - order.indexOf(b.color as Color)
+      )
+    : colorConfigs;
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent className="w-full">
+        <section className="grid grid-cols-4 gap-4 w-full">
+          {sortedConfigs.map(({ color, bgClass, hoverClass, activeClass }) => (
+            <Button
+              key={color}
+              className={cn(
+                `${bgClass} h-44 ${hoverClass} text-white font-bold text-4xl`,
+                order.includes(color) && activeClass
+              )}
+              onClick={() => setOrder([...order, color as Color])}
+            >
+              {order.includes(color) ? order.indexOf(color) + 1 : ""}
+            </Button>
+          ))}
+        </section>
+      </CardContent>
+      <CardFooter>
+        <Button variant="destructive" onClick={() => setOrder([])}>
+          Reset
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
